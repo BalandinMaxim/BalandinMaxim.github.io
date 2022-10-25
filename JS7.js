8a1b6b5c022ff0fa842afa71f01c7a6ed512be76
BalandinMaxim <112868629+BalandinMaxim@users.noreply.github.com>
2022-10-25 17:26:09 +0300

    Create JS7.js

--- JS7.js
+++ JS7.js
@@ -1,23 +1,22 @@
-var document;
-var window;
-var alert;
-var console;
-var $;
-$(document).ready(function () {
-    $(".slider").slick({
-        arrows: true,
-        dots: true,
-        responsive: [
-            {
-                breakpoint: 750,
-                settings: {
-                  slidesToScroll: 1,
-                    slidesToShow: 2
+
+jQuery(function ($) {
+    $(document).ready(function () {
+        $(".slider").slick({
+            arrows: true,
+            dots: true,
+            responsive: [
+                {
+                    breakpoint: 560,
+                    settings: {
+                        arrows: true,
+                        dots: true,
+                        slidesToScroll: 1,
+                        slidesToShow: 2
+                    }
                 }
-            }
-        ],
-        slidesToScroll: 1,
-        slidesToShow: 4,
-        waitForAnimate: false
+            ],
+            slidesToScroll: 1,
+            slidesToShow: 4
+        });
     });
 });
