94a739a08eafd63f910a2cc81d04775e70cbb9aa
BalandinMaxim <112868629+BalandinMaxim@users.noreply.github.com>
2022-10-25 17:53:53 +0300

    Update JS7.js

--- JS7.js
+++ JS7.js
@@ -1,22 +1,22 @@
-
-jQuery(function ($) {
-    $(document).ready(function () {
-        $(".slider").slick({
-            arrows: true,
-            dots: true,
-            responsive: [
-                {
-                    breakpoint: 560,
-                    settings: {
-                        arrows: true,
-                        dots: true,
-                        slidesToScroll: 1,
-                        slidesToShow: 2
-                    }
+var document;
+var window;
+var alert;
+var console;
+var $;
+$(document).ready(function () {
+    $(".slider").slick({
+        arrows: true,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 2
                 }
-            ],
-            slidesToScroll: 1,
-            slidesToShow: 4
-        });
+            }
+        ],
+        slidesToScroll: 4,
+        slidesToShow: 4,
+        waitForAnimate: false
     });
 });
